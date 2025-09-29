95fd7ccc2b48ed80e36b3bbe38c1332ae99f9d7b
Stephen_A <ankustephen99@gmail.com>
2025-09-29 10:32:45 -0400

    Added options for user to input middle name or suffix

--- src/main/java/com/pluralsight/FullNameApplication.java
+++ src/main/java/com/pluralsight/FullNameApplication.java
@@ -7,36 +7,56 @@ public class FullNameApplication {
         System.out.println("Please enter your first name:");
         Scanner nameScanner = new Scanner(System.in);
         String firstName = nameScanner.nextLine();
+        firstName = firstName.trim();
         System.out.println("Please enter your last name:");
         String lastName = nameScanner.nextLine();
+        lastName = lastName.trim();
         System.out.println("Do you have a middle name? (yes/no):");
         String middleNameCheck = nameScanner.nextLine();
         boolean middleNameBoolean = false;
+        String middleName = "";
         if (middleNameCheck.equalsIgnoreCase("yes") || middleNameCheck.equalsIgnoreCase("y")) {
             middleNameBoolean = true;
         } else {
             middleNameBoolean = false;
         }
 
-        if (middleNameBoolean = true) {
+        if (middleNameBoolean == true) {
             System.out.println("Please enter your middle name:");
-            String middleName = nameScanner.nextLine();
+            middleName = nameScanner.nextLine();
         }
+        middleName = middleName.trim();
 
         System.out.println("Do you have a suffix? (yes/no)");
         String suffixCheck = nameScanner.nextLine();
         boolean suffixBoolean = false;
+        String suffixName = "";
         if (suffixCheck.equalsIgnoreCase("yes") || suffixCheck.equalsIgnoreCase("y")) {
             suffixBoolean = true;
         } else {
             suffixBoolean = false;
         }
 
-        if (suffixBoolean = true) {
+        if (suffixBoolean == true) {
             System.out.println("Please enter your suffix:");
-            String middleName = nameScanner.nextLine();
+            suffixName = nameScanner.nextLine();
         }
 
+        suffixName = suffixName.trim();
+        suffixName = ", " + suffixName;
 
+        String fullName = "";
 
-}
+        if (suffixBoolean && middleNameBoolean) {
+            fullName = firstName + " " + middleName + " " + lastName + suffixName;
+        } else if (suffixBoolean && middleNameBoolean == false) {
+            fullName = firstName + " " + lastName + suffixName;
+        } else if (suffixBoolean == false && middleNameBoolean) {
+            fullName = firstName + " " + middleName + " " + lastName;
+        } else {
+            fullName = firstName + " " + lastName;
+        }
+
+        System.out.println("You full name is: "+ fullName);
+        }
+    }
